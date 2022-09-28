4bcf54b04260853a4224167128da7f1ebb90a154
felipepperez <fpproducoes@gmail.com>
2022-09-27 20:13:49 -0400

    Aula 27092022

---
+++ src/20220927/read.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+include_once("connection.php");
+
+if($_SERVER["REQUEST_METHOD"]==="GET"){
+    
+    $query = "SELECT id, nome, usuario FROM usuarios";
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    
+    $users = Array();
+    
+    while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+        array_push($users,$line);
+    }
+    
+    echo json_encode($users);
+
+    exit();
+}
+
+http_response_code(400);
