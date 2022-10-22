e5ae5deccb50be8a8b24c656b3dac11f12d9d7d0
felipepperez <fpproducoes@gmail.com>
2022-10-21 21:33:40 -0400

    aula 21102022

--- src/20221018/src/model/Usuario.php
+++ src/20221018/src/model/Usuario.php
@@ -13,12 +13,7 @@ class Usuario
     private $tipo;
     private $administrador;
 
-    private $enumStatus = array(null => null, "create" => "1", "remove_red_eye" => "2", "clear" => "3");
-    private $enumTipo = array(
-        null => null, "supervisor_account" => "1",
-        "person" => "2"
-    );
-    private $enumAdministrador = array(null => null, "1" => true, "0" => false);
+    private $enumAdministrador = array(null => null, "t" => true, "f" => false);
 
     private $conexao;
 
@@ -67,7 +62,7 @@ class Usuario
 
     public function setStatus($status)
     {
-        $this->status = $this->enumStatus[$status];
+        $this->status = $status;
     }
     public function getStatus()
     {
@@ -76,7 +71,7 @@ class Usuario
 
     public function setTipo($tipo)
     {
-        $this->tipo = $this->enumTipo[$tipo];
+        $this->tipo = $tipo;
     }
     public function getTipo()
     {
@@ -98,18 +93,99 @@ class Usuario
         if ($this->id != null)
             return false;
 
-        $query = "INSERT INTO usuarios (nome,rgm,usuario,senha,status,tipo,administrador,created,modified) VALUES ('".$this->nome."','".$this->rgm."','".$this->nomeUsuario."','".md5(sha1($this->senha))."','remove_red_eye','person',false,'" . date(DATE_ATOM) . "','" . date(DATE_ATOM) . "')";
+        $query = "INSERT INTO usuarios (nome,rgm,usuario,senha,status,tipo,administrador,created,modified) VALUES ('" . $this->nome . "','" . $this->rgm . "','" . $this->nomeUsuario . "','" . md5(sha1($this->senha)) . "','remove_red_eye','person',false,'" . date(DATE_ATOM) . "','" . date(DATE_ATOM) . "')";
 
         $result = pg_query($query);
 
-        if($result)
+        if ($result)
             $this->id = pg_last_oid($result);
+
+        return $result;
+    }
+
+    public function ler($id = null)
+    {
+        if ($id == null) {
+            $sql = "SELECT * FROM usuarios";
+        } else {
+            $sql = "SELECT * FROM usuarios WHERE id='$id'";
+        }
+        $returnValue = array();
+
+        $result = pg_query($sql);
+
+        while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+            array_push($returnValue, $line);
+        }
+
+        return $returnValue;
+    }
+
+    public function getUsuario($id)
+    {
+        $result = $this->ler($id);
+        if (empty($result))
+            return 0;
+
+        $result = $result[0];
+
+        $this->id = $id;
+        $this->setNome($result["nome"]);
+        $this->setRGM($result["rgm"]);
+        $this->setNomeUsuario($result["usuario"]);
+        $this->setSenha($result["senha"]);
+        $this->setTipo($result["tipo"]);
+        $this->setStatus($result["status"]);
+        $this->setAdministrador($result["administrador"]);
+
+        return $this;
+    }
+
+    public function atualizar()
+    {
+        error_reporting(E_ERROR | E_PARSE);
+
+        if ($this->id == null)
+            return false;
+
+        $query = "UPDATE usuarios SET nome= '" . $this->nome . "', rgm= '" . $this->rgm . "', usuario= '" . $this->usuario . "', senha= '" . $this->senha . "', status= '" . $this->status . "', tipo= '" . $this->tipo . "', administrador= '" . $this->administrador . "', modified= '" . date(DATE_ATOM) . "' WHERE id= '" . $this->id . "'";
+
+        $result = pg_query($query);
+
+        return $result;
+    }
+
+    public function deletar()
+    {
+        if ($this->id == null)
+            return false;
+
+        $query = "DELETE FROM usuarios WHERE id='" . $this->id . "'";
+
+        $result = pg_query($query);
+        
+        if($result)
+            $this->id = null;
 
         return $result;
     }
 }
 
 
-$usuario = new Usuario("Felipe","46763","fpp","12345","remove_red_eye","supervisor_account",true);
+//Instanciar um novo usuario com dados
+//$usuario = new Usuario("Felipe", "46763", "fpp", "12345", "remove_red_eye", "supervisor_account", true);
+//Inserir no banco de dados
+//$usuario->criar();
 
-$usuario->criar();
+//Instanciar um novo usuario sem dados
+//$usuario = new Usuario();
+//Pegar as informações do usuario pelo seu ID
+//$usuario->getUsuario(2);
+
+//Setar informações do usuario
+//$usuario->setNome("Felipe");
+//Atualizar informações no banco de dados
+//$usuario->atualizar();
+
+//Deletar o usuario
+//$usuario->deletar();
